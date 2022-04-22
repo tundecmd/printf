69e777fe3061d90dc73233d3f355b3758f2af99b
Gbemividz0n <43783192+Gbemividz0n@users.noreply.github.com>
2022-04-22 17:40:07 +0100

    Update write_funcs.c

--- write_funcs.c
+++ write_funcs.c
@@ -8,7 +8,6 @@
  * On error, -1 is returned, and errno is set appropriately.
  * Description: _putchar uses a local buffer of 1024 to call write
  * as little as possible
- * 
  */
 int _putchar(char c)
 {
